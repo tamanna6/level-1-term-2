ab3d877c75edf9fabdb7a125ea5930b3175334b0
Tamanna Sultana <123634557+tamanna6@users.noreply.github.com>
2023-01-27 20:33:19 +0600

    check upper and lower limits

---
+++ check upper and lower limits.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    cout<<"the maximum limit of int data type:"<<INT_MAX<<endl;
+    cout<<"the minimum limit of int data type:"<<INT_MIN<<endl;
+return 0;
+
+
+
+}
